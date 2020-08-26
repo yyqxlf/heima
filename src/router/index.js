c63186027784ef93eb1045ea8d5ca7ad6a0d69a1
yyqxlf <2362908450@qq.com>
2020-08-26 17:19:17 +0800

    初始化提交

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//导入组件
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path:'/login',component:Login},
+  {path:'/register',component:Register}
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
